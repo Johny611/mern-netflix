ec69653189cbcf71d639412c9c83638bcee8ee2c
Johny611 <63981669+Johny611@users.noreply.github.com>
2022-07-15 15:17:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+dotenv.config();
+
+const corsOptions = {
+  origin: "https://mern-netflix.netlify.app",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+const port = 80;
+
+app.listen(port, () => {
+  console.log(`Backend server is running! on port ${port}`);
+});
